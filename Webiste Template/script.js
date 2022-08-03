1551a873ad46333f11e2d4b565796c10d56be387
Jessel _Zapanta <90964567+JesselZapanta@users.noreply.github.com>
2022-08-03 10:30:50 +0800

    do media queries

--- Webiste Template/script.js
+++ Webiste Template/script.js
@@ -2,6 +2,14 @@ const mainContainer = document.querySelector(".container");
 const mainContent = document.querySelectorAll(".main-content");
 const links = document.querySelectorAll(".link-btn");
 
+const sideBar = document.querySelector(".sidebar");
+const buttons = document.querySelector(".nav-btn");
+
+buttons.addEventListener("click", function () {
+  sideBar.classList.toggle("active");
+  buttons.classList.toggle("active");
+});
+
 mainContainer.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   console.log(id);
@@ -10,6 +18,8 @@ mainContainer.addEventListener("click", function (e) {
     links.forEach(function (btn) {
       btn.classList.remove("active");
       e.target.classList.add("active");
+      sideBar.classList.remove("active");
+      buttons.classList.remove("active");
     });
     // hide the content
     mainContent.forEach(function (content) {
